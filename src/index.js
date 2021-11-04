a006eedc8716a1c9c9577a90c220ae78c02bef28
John Harold Apolinario <apolinariojohnharold05@gmail.com>
2021-11-05 04:05:58 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+const app = initializeApp({
+    apiKey: "AIzaSyB2Kf4BH-rzmq6T4IZrRRuEoA2WmulcrLY",
+    authDomain: "learnfirebase-7de50.firebaseapp.com",
+    projectId: "learnfirebase-7de50",
+    storageBucket: "learnfirebase-7de50.appspot.com",
+    messagingSenderId: "1092881694951",
+    appId: "1:1092881694951:web:115e3864d684e46fc5e242",
+    measurementId: "G-XTK254WG5D"
+})
+
+
+console.log(app)
